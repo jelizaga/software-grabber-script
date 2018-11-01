237f01b1b750b1e60cd34d54eab1a1c481b258a0
Joel Elizaga <joelizaga@hotmail.com>
2018-11-01 11:35:02 -0700

    Added readme and VPN.

---
+++ README.md
@@ -0,0 +1,52 @@
+#Xubuntu Start-Up Software Installer
+
+##Installs...
+
+###Utilities
+- Terminator
+- Synaptic Package Manager
+- Gnome Disk Utility
+- Filezilla
+- VirtualBox
+- Docky
+- Sensors
+- Wireshark
+- Shutter
+- screenfetch
+- PIA VPN (installer included with script)
+
+###Software Development
+- Sublime
+- Ruby
+- Ruby Dev
+- git
+- vim
+
+###Media Creation
+- Krita
+- Inkscape
+- DarkTable
+- OBS Studio
+- Audacity
+- ffmpeg
+- Gimp
+- Blender
+
+###Media Consumption
+- Spotify
+- Quod Libet
+- VLC
+
+###Fun
+- higan
+- PCSXR
+- PCSX2
+- Dolphin Emulator
+
+###Aesthetics
+- Papirus
+
+## Removes
+
+###Media Consumption
+- Parole Media Player
--- startup.sh
+++ startup.sh
@@ -12,15 +12,20 @@ sudo apt-add-repository ppa:dolphin-emu/ppa
 
 sudo add-apt-repository ppa:kritalime/ppa
 
+sudo add-apt-repository ppa:papirus/papirus
+
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
 sudo apt update
 
-sudo apt install sublime-text krita virtualbox pcsx2 pcsxr dolphin-emu lm-sensors spotify-client darktable git inkscape obs-studio quodlibet audacity docky ruby ruby-dev ffmpeg wireshark vlc shutter blender gimp terminator screenfetch vim synaptic gnome-disk-utility higan filezilla terminator
+sudo apt install sublime-text papirus-icon-theme krita virtualbox pcsx2 pcsxr dolphin-emu lm-sensors spotify-client darktable git inkscape obs-studio quodlibet audacity docky ruby ruby-dev ffmpeg wireshark vlc shutter blender gimp terminator screenfetch vim synaptic gnome-disk-utility higan filezilla terminator
 
-sudo apt remove parole
+sudo apt remove parole 
 
 sudo apt autoremove
 
 sudo sensors-detect
+
+./pia-v81-installer-linux.sh 
+
