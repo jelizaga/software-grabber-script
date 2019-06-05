b79a7287643a1d3a41431b086d213297be5afca9
Joel Elizaga <joelizaga@hotmail.com>
2019-06-05 16:36:55 -0700

    Assumes -y on all installs, added DisplayCAL.

--- startup.sh
+++ startup.sh
@@ -7,108 +7,111 @@ sudo apt update
 
 # UTILITIES ###################################################################
 # TERMINATOR
-sudo apt install terminator
+sudo apt install -y terminator
 # SYNAPTIC PACKAGE MANAGER
-sudo apt install synaptic
+sudo apt install -y synaptic
 # GNOME DISK UTILITY
-sudo apt install gnome-disk-utility
+sudo apt install -y gnome-disk-utility
 # FILEZILLA
-sudo apt install filezilla
+sudo apt install -y filezilla
 # VIRTUALBOX
-sudo apt install virtualbox
+sudo apt install -y virtualbox
 # CHROMIUM
-sudo apt install chromium-browser
+sudo apt install -y chromium-browser
 # WIRESHARK
-sudo apt install wireshark
+sudo apt install -y wireshark
 # SCREENFETCH
-sudo apt install screenfetch
+sudo apt install -y screenfetch
 # SHUTTER
-sudo apt install shutter
+sudo apt install -y shutter
 # DOCKY
-sudo apt install docky
+sudo apt install -y docky
 # SENSORS
-sudo apt install lm-sensors
+sudo apt install -y lm-sensors
+# DISPLAYCAL
+wget https://displaycal.net/download/xUbuntu_19.04/amd64/DisplayCAL.deb
+sudo apt install -y ./DisplayCAL.deb
 
 # SOFTWARE DEVELOPMENT ########################################################
 # SUBLIME
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt install apt-transport-https
+sudo apt install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
-sudo apt install sublime-text
+sudo apt install -y sublime-text
 # RUBY
-sudo apt install ruby 
+sudo apt install -y ruby 
 # RUBY DEV
 ruby ruby-dev 
 # GIT
-sudo apt install git
+sudo apt install -y git
 # VIM
-sudo apt install vim
+sudo apt install -y vim
 
 # MEDIA CREATION ##############################################################
 # FFMPEG
-sudo apt install ffmpeg
+sudo apt install -y ffmpeg
 # AUDACITY
-sudo apt install audacity
+sudo apt install -y audacity
 # YOUTUBE-DL
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
 # GIMP
-sudo apt install gimp
+sudo apt install -y gimp
 # BLENDER
-sudo apt install blender
+sudo apt install -y blender
 # OBS
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
-sudo apt install obs-studio
+sudo apt install -y obs-studio
 # KRITA
 sudo add-apt-repository ppa:kritalime/ppa
 sudo apt update
-sudo apt install krita
+sudo apt install -y  krita
 # DARKTABLE
-sudo apt install darktable
+sudo apt install -y darktable
 # INKSCAPE 
 sudo add-apt-repository ppa:inkscape.dev/stable
 sudo apt update
-sudo apt install inkscape
+sudo apt install -y inkscape
 
 # MEDIA CONSUMPTION ###########################################################
 # VLC
-sudo apt install vlc
+sudo apt install -y vlc
 # QUODLIBET
 sudo add-apt-repository ppa:lazka/ppa
 sudo apt update
-sudo apt install quodlibet
+sudo apt install -y quodlibet
 # SPOTIFY
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update
-sudo apt install spotify-client
+sudo apt install -y spotify-client
 
 
 # FUN #########################################################################
 # DOLPHIN
 sudo apt-add-repository ppa:dolphin-emu/ppa
 sudo apt update
-sudo apt install dolphin-emu
+sudo apt install -y dolphin-emu
 # HIGAN
-sudo apt install higan
+sudo apt install -y higan
 # RETROARCH
 sudo add-apt-repository ppa:libretro/stable
 sudo apt update
-sudo apt install retroarch*
+sudo apt install -y retroarch*
 # PCSXR
-sudo apt install pcsxr
+sudo apt install -y pcsxr
 # PCSX2
-sudo apt install pcsx2
+sudo apt install -y pcsx2
 
 # AESTHETICS ##################################################################
 # PAPIRUS ICONS
 sudo add-apt-repository ppa:papirus/papirus
 sudo apt update
-sudo apt install papirus-icon-theme
+sudo apt install -y papirus-icon-theme
 # DOCKY
-sudo apt install docky
+sudo apt install -y docky
 
 # REMOVE... ###################################################################
 # PAROLE
